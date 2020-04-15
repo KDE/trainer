f0237f434ccee8e0db72b336b38b8cdbf5fa8c72
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2020-04-15 11:11:18 +0200

    add license headers for all files missing it

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2020 (c) 2020 Matthieu Gallien <matthieu_gallien@yahoo.fr>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 $XGETTEXT $(find . -a \( -name \*.qml -o -name \*.cpp -o -name \*.h \)) -o $podir/trainer.pot
